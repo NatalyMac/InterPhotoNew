40beebe789e30001cb9fbe03e3aa3734293a24d8
Nataly Kulychyk <kulychyk.nataliia@travers-group.com>
2016-06-28 10:37:15 +0300

    fixed conf

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=photoTest',
+    'username' => 'root',
+    'password' => 'AcidBurn',
+    'charset' => 'utf8',
+    //'enableQueryCache'      => true,
+    //'queryCache'            => 'cache',
+    //'queryCacheDuration'    => 600,
+    //'enableSchemaCache'     => true,
+    //'schemaCache'           => 'cache',
+    //'schemaCacheDuration'   => 3600,
+];
